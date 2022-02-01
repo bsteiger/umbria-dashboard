beb2d5637677d8d38bfc44d684b2307c052cf30f
bsteiger <>
2022-02-01 13:02:19 -0600

    single coin price fetch working

--- src/logic/coingecko.js
+++ src/logic/coingecko.js
@@ -1,65 +1,58 @@
 /** Module to use CoinGecko API */
 class CoinGecko {
 	baseUrl = "https://api.coingecko.com/api/v3/";
-	// coinList = null;
-
-	constructor() {
-		console.log("Hi From CG API Constructor👋");
-	}
-
-	/** Internal method to do the api fetch and handle errors (eventually) */
-	async _apiFetch(endpoint) {
-		let response = await fetch(endpoint);
-		console.log(response);
-		let json = await response.json();
-		return json;
-	}
+	currency = "usd";
 
 	async updateCoinList() {
 		console.log("Updating Coin List...");
 		this.coinList = await this.getCoinList();
-		console.log("Updated Coin List.");
-		console.log(this.coinList);
 		return;
 	}
 
 	async getCoinList() {
 		console.log("Getting Coin List...");
 		const endpoint = `${this.baseUrl}coins/list?include_platform=true`;
-		// const endpoint = https://api.coingecko.com/api/v3/coins/list?include_platform=true
-		const resp = await fetch(endpoint);
+		const resp = await _fetch(endpoint);
 		const coinList = await resp.json();
-		console.log("Got Coin List.", this.coinList);
+		console.log("Got Coin List.", `${coinList.length} coins.`);
 		return coinList;
 	}
 
 	//* Return the first id for a given symbol
 	async getIdBySymbol(symbol) {
-		console.log(`Getting ID for ${symbol}`);
 		if (!this.coinList) {
 			await this.updateCoinList();
-			console.log(this.coinList);
 		}
 		const coinList = await this.coinList;
 		const coin = coinList.filter(
 			(c) => c.symbol.toLowerCase() === symbol.toLowerCase()
 		)[0];
-		console.log(coin);
-		return coin;
+		return coin.id;
 	}
 
 	async getIdByContractAddress(address, platform) {}
 
-	async getPriceBySymbol(symbol) {
-		console.log(`Getting price for ${symbol}`);
-		const id = await this.getIdBySymbol(symbol);
-
-		return;
+	async getPriceById(id, currency = this.currency) {
+		console.log(`Getting price for id:${id}`);
+		const endpoint = `${this.baseUrl}coins/${id}`;
+		const resp = await _fetch(endpoint);
+		const json = await resp.json();
+		const price = json.market_data.current_price[this.currency];
+		return price;
 	}
 
-	async getCurrentUmbrPrice() {
-		null;
+	async getPriceBySymbol(symbol) {
+		console.log(`Getting price for symbol:${symbol}`);
+		const id = await this.getIdBySymbol(symbol);
+		return await this.getPriceById(id);
 	}
 }
 
+/** Internal function to do the api fetch and handle errors (eventually) */
+async function _fetch(endpoint) {
+	console.log("Req:", endpoint);
+	let response = await fetch(endpoint);
+	console.log(response);
+	return response;
+}
 export default CoinGecko;
