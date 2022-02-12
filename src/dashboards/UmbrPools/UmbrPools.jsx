5befe94280d74c6470970bfc630b598fecd7b4a6
bsteiger <>
2022-02-12 15:56:31 -0600

    added button group to select averages

--- src/dashboards/UmbrPools/UmbrPools.jsx
+++ src/dashboards/UmbrPools/UmbrPools.jsx
@@ -12,21 +12,55 @@ import {
 } from "recharts";
 import React, { useState, useEffect } from "react";
 import { getUmbrPoolAprBreakdown } from "./umbrPoolLogic";
+import TableAllApys from "../../components/TableAllApys";
+import ButtonGroup from "../../components/ButtonGroup";
 
 function UmbrPools() {
-  const [umbrPoolData, setUmbrPoolData] = useState([]);
+  const [umbrPoolData, setUmbrPoolData] = useState({});
+  const [selectedAvg, setSelectedAvg] = useState(7);
+  const headers = [
+    { text: "Asset", styles: {} },
+    { text: "Network", styles: {} },
+    { text: "Bridge", styles: {} },
+    { text: "APY", styles: { textAlign: "right" }, sortIconLocation: "left" },
+  ];
+
+  function umbrPoolAprFromPoolData(network) {
+    if (!Object.keys(umbrPoolData).length) return null;
+    return Object.values(umbrPoolData).reduce((a, b) => a + b);
+  }
+
+  /** Sets  */
+  function handleAvgSelect(value) {
+    console.log(`Setting for ${value}d average`);
+    setSelectedAvg(value);
+  }
 
   useEffect(() => {
     async function getUmbrData() {
       const poolData = await getUmbrPoolAprBreakdown("ethereum", 7);
-      return setUmbrPoolData(poolData);
+      console.log(poolData);
+      setUmbrPoolData(poolData);
     }
     getUmbrData();
   }, []);
 
+  const buttons = [
+    { text: "30d", value: 30 },
+    { text: "14d", value: 14 },
+    { text: "7d", value: 7 },
+    { text: "1d", value: 1 },
+  ];
+
+  /// ((((RENDER)))) ///
   return (
     <div className="overview-page">
       <div className="container">
+        <ButtonGroup
+          buttons={buttons}
+          onClick={(val) => handleAvgSelect(val)}
+          activeValue={selectedAvg}
+        />
         <div className="row mt-2">
           <div className="col">
             <div className="card">
@@ -36,6 +70,49 @@ function UmbrPools() {
                   Estimated rewards for staking UMBR (broken down by asset)
                 </p>
                 <RechartChart />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">
+                  Staking Rewards for UMBR on Ethereum Network
+                </h5>
+                <p className="card-subtitle mb-2">
+                  {`Estimated rewards for staking UMBR: ${(
+                    umbrPoolAprFromPoolData("ethereum") * 100
+                  ).toFixed(2)}%`}
+                </p>
+                <table className="table table-sm">
+                  <thead>
+                    <tr>
+                      {headers.map((header) => (
+                        <th
+                          key={header.text}
+                          style={{ ...header.styles }}
+                          scope="col"
+                        >
+                          {header.text}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.map((entry) => {
+                      return (
+                        <tr>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                          <td></td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>{" "}
               </div>
             </div>
           </div>
