c749f118ff14f89d0adebbea1d72229bbdaa9041
bsteiger <>
2022-02-19 14:43:40 -0600

    wrapped body class properly

--- src/components/allNetworksAllApys.jsx
+++ src/components/allNetworksAllApys.jsx
@@ -7,7 +7,7 @@ function AllNetworksAllApys({ data }) {
   }
 
   return (
-    <div className="row row-cols-2 row-cols-lg-2 mt-2">
+    <div className="row row-cols-2  mt-2">
       {NETWORKS.map((network) => {
         // console.log(data);
         return (
@@ -26,18 +26,20 @@ function BridgeApyCard({ bridgeData, network }) {
   return (
     <div className="col">
       <div className="card mb-2">
-        <h6 className="card-body">{network} Network</h6>
-        {bridgeData.map((pool) => {
-          return (
-            <div key={pool.asset + pool.bridge}>
-              <span>{pool.asset}</span>
-              <div style={{ fontSize: ".75rem" }}>
-                {pool.bridge ? pool.bridge : ""}
+        <div className="card-body ">
+          <h6>{network} Network</h6>
+          {bridgeData.map((pool) => {
+            return (
+              <div className="apy-card-body" key={pool.asset + pool.bridge}>
+                <span>{pool.asset}</span>
+                <div style={{ fontSize: ".75rem" }}>
+                  {pool.bridge ? pool.bridge : ""}
+                </div>
+                <pre>{(pool.apy * 100).toFixed(1)}% APY</pre>
               </div>
-              <pre>{(pool.apy * 100).toFixed(1)}% APY</pre>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
