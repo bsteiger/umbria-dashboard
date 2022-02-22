a4b34e7dcce61490b176adca5131cb01ab0048bc
bsteiger <>
2022-02-22 16:43:37 -0600

    added sort to the apy table

--- src/components/TableAllApys.jsx
+++ src/components/TableAllApys.jsx
@@ -2,30 +2,65 @@ import React, { useState, useEffect } from "react";
 import { SortUpIcon, SortDownIcon } from "./icons";
 /** Table component containing all APYs for all Networks */
 function TableAllApys({ data }) {
+  const [sortKey, setSortKey] = useState("apy");
+  const [sortDirection, setSortDirection] = useState("desc");
+  const [sortedData, setSortedData] = useState(data);
+
   const headers = [
-    { text: "Asset", styles: {} },
-    { text: "Network", styles: {} },
-    { text: "Bridge", styles: {} },
-    { text: "APY", styles: { textAlign: "right" }, sortIconLocation: "left" },
+    { text: "Asset", key: "asset", styles: {}, sortIconLocation: "right" },
+    { text: "Network", key: "network", styles: {}, sortIconLocation: "right" },
+    { text: "Bridge", key: "bridge", styles: {}, sortIconLocation: "right" },
+    {
+      text: "APY",
+      key: "apy",
+      styles: { textAlign: "right" },
+      sortIconLocation: "left",
+    },
   ];
 
+  function handleSort(key) {
+    if (key != sortKey) {
+      setSortKey(key);
+      setSortDirection("desc");
+    } else {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    }
+  }
+
   useEffect(() => {
-    // Stuff to do when the component loads
-  }, []);
+    setSortedData(_.orderBy(data, sortKey, sortDirection));
+  }, [data, sortKey, sortDirection]);
 
   return (
     <table className="table table-sm">
       <thead>
         <tr>
           {headers.map((header) => (
-            <th key={header.text} style={{ ...header.styles }} scope="col">
+            <th
+              onClick={() => handleSort(header.key)}
+              key={header.key}
+              style={{ cursor: "pointer", ...header.styles }}
+              scope="col"
+            >
+              {header.key === sortKey &&
+                header.sortIconLocation === "left" &&
+                sortDirection === "asc" && <SortUpIcon />}
+              {header.key === sortKey &&
+                header.sortIconLocation === "left" &&
+                sortDirection === "desc" && <SortDownIcon />}
               {header.text}
+              {header.key === sortKey &&
+                header.sortIconLocation === "right" &&
+                sortDirection === "asc" && <SortUpIcon />}
+              {header.key === sortKey &&
+                header.sortIconLocation === "right" &&
+                sortDirection === "desc" && <SortDownIcon />}
             </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((entry) => {
+        {sortedData.map((entry) => {
           return (
             <tr key={entry.asset + entry.network + entry.bridge}>
               <td>{entry.asset}</td>
