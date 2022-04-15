2151266dc24fe3778f533e03255e97185dbb61a8
bsteiger <>
2022-04-15 00:31:16 -0500

    new types.ts for common types & interfaces

--- src/components/TableAllApys.tsx
+++ src/components/TableAllApys.tsx
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { SortUpIcon, SortDownIcon } from "./icons";
-import { Bridge } from "../constants/networks";
+import { ApyData } from "../constants/types";
 import _ from "lodash";
 
-interface ApyData {
-  apy: number;
-  asset: string;
-  bridge: Bridge;
-  network: string;
-}
 type SortDirection = "asc" | "desc";
-type Props = { data: any };
+type Props = { data: ApyData[] | undefined };
 
 /** Table component containing all APYs for all Networks */
 function TableAllApys({ data }: Props) {
   const [sortKey, setSortKey] = useState<string>("apy");
   const [sortDirection, setSortDirection] = useState<SortDirection>("desc");
-  const [sortedData, setSortedData] = useState<ApyData[]>(data);
+  const [sortedData, setSortedData] = useState<ApyData[] | undefined>(data);
 
   const headers: {
     text: string;
@@ -77,18 +71,19 @@ function TableAllApys({ data }: Props) {
         </tr>
       </thead>
       <tbody>
-        {sortedData.map((entry) => {
-          return (
-            <tr key={entry.asset + entry.network + entry.bridge}>
-              <td>{entry.asset}</td>
-              <td>{entry.network}</td>
-              <td>{entry.bridge}</td>
-              <td style={{ textAlign: "right" }}>{`${(100 * entry.apy).toFixed(
-                2
-              )}%`}</td>
-            </tr>
-          );
-        })}
+        {sortedData &&
+          sortedData.map((entry) => {
+            return (
+              <tr key={entry.asset + entry.network + entry.bridge}>
+                <td>{entry.asset}</td>
+                <td>{entry.network}</td>
+                <td>{entry.bridge}</td>
+                <td style={{ textAlign: "right" }}>{`${(
+                  100 * entry.apy
+                ).toFixed(2)}%`}</td>
+              </tr>
+            );
+          })}
       </tbody>
     </table>
   );
